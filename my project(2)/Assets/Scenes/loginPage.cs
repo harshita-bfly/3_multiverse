f59c390467ceb93ed98d95b312fb689330a220a7
jessica banka <jessicabanka3081@gmail.com>
2022-09-18 03:26:40 +0530

    deletion and merge

---
+++ my project(2)/Assets/Scenes/loginPage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loginPage : MonoBehaviour
+{
+    // Start is
+    // called before the first frame update
+
+    public float time = 1.0f;
+    void Start()
+    {
+        StartCoroutine(login());
+
+    }
+
+    // Update is called once per frame
+    
+    IEnumerator login()
+    {
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene(2);
+    }
+}
+
